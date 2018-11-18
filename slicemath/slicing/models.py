665842d94b6f9ba414b2848528cca27420eabdf6
forsman5 <jrforsman@gmail.com>
2018-11-18 16:43:04 -0500

    architect-ed the models

--- slicemath/slicing/models.py
+++ slicemath/slicing/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Sessions(models.Model):
+    # many Members
+    # many pizzas
+
+# Members
+    # for every member, one session
+
+# pizzas
+    # many Toppings
+    # one Sauce
+    # one crust
+    # one session
+
+# Toppings
+    # name
+    # many pizzas
+
+# Crusts
+    # name
+    # many pizzas
+
+# Sauces
+    # name
+    # many pizzas
+
+# Sizes
+    # name
+    # many pizzas
