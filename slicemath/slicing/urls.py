68049a132209fea40da5c8a4b3286c14d21321e3
forsman5 <jrforsman@gmail.com>
2018-11-18 20:09:43 -0500

    refactored to post and get to one address

--- slicemath/slicing/urls.py
+++ slicemath/slicing/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('newSession/', views.newSession, name='newSession'),
     path('session/<int:id>', views.session, name='session'),
 ]
--- slicemath/slicing/views.py
+++ slicemath/slicing/views.py
@@ -4,30 +4,30 @@ from django.urls import reverse
 from . import forms
 
 def index(request):
-    createSession = forms.NewSessionForm()
+    if request.method == 'POST' or request.method == 'post':
+        # create a form instance and populate it with data from the request:
+        form = forms.NewSessionForm(request.POST)
 
-    return render(request, 'index.html', {'form': createSession})
+        # check whether it's valid:
+        if form.is_valid():
+            # create a new session
+            sessionId = 0
+
+            # create the new member of that session
+
+            # return the rendered, new session page
+            return HttpResponseRedirect(reverse('session', args = [sessionId]))
+
+        else:
+            # TODO: print an error and request resumbit
+            pass
+    else:
+        createSession = forms.NewSessionForm()
+
+        return render(request, 'index.html', {'form': createSession})
 
 def session(request, id):
     # get session from db
     session = None
 
     return render(request, 'session.html', {'session': session})
-
-def newSession(request):
-    # create a form instance and populate it with data from the request:
-    form = forms.NewSessionForm(request.POST)
-
-    # check whether it's valid:
-    if form.is_valid():
-        # create a new session
-        sessionId = 0
-
-        # create the new member of that session
-
-        # return the rendered, new session page
-        return HttpResponseRedirect(reverse('session', args = [sessionId]))
-
-    else:
-        # TODO: redirect
-        pass
