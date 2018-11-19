adf582ceed9b5dee2a48f6df419e623b9fcd6f28
forsman5 <jrforsman@gmail.com>
2018-11-18 21:07:30 -0500

    dwayne

--- slicemath/slicing/forms.py
+++ slicemath/slicing/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class NewSessionForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=50, widget=forms.TextInput(attrs={'class': "form-control"}))
+    name = forms.CharField(label='Your Name', max_length=50, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'Dwayne Haskins'}))
 
 class JoinSessionForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=50, widget=forms.TextInput(attrs={'class': "form-control"}))
+    name = forms.CharField(label='Your Name', max_length=50, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'Dwayne Haskins'}))
     sessionCode = forms.CharField(label='Session Code', max_length=50, widget=forms.TextInput(attrs={'class': "form-control"}))
