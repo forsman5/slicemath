96b998f02630ac3e46fda0bd502c897240b29dc5
forsman5 <jrforsman@gmail.com>
2018-11-18 20:46:00 -0500

    finished form handling

--- slicemath/slicing/forms.py
+++ slicemath/slicing/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class NewSessionForm(forms.Form):
-    yourName = forms.CharField(label='Your Name', max_length=50)
+    name = forms.CharField(label='Your Name', max_length=50)
 
 class JoinSessionForm(forms.Form):
-    yourName = forms.CharField(label='Your Name', max_length=50)
+    name = forms.CharField(label='Your Name', max_length=50)
     sessionCode = forms.CharField(label='Session Code', max_length=50)
--- slicemath/slicing/views.py
+++ slicemath/slicing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from . import forms
+from . import models
 
 def index(request):
     if request.method == 'POST' or request.method == 'post':
@@ -12,14 +13,16 @@ def index(request):
             # check whether it's valid:
             if form.is_valid():
                 # create a new session
-                sessionId = 0
+                session = models.Session()
+                session.save()
 
-                # create the new member of that session
+                member = models.Member.objects.create(name=form.cleaned_data['name'], session=session)
 
                 # return the rendered, new session page
-                return HttpResponseRedirect(reverse('session', args = [sessionId]))
+                return HttpResponseRedirect(reverse('session', args = [session.id]))
 
             else:
+                print("FAIL")
                 # TODO: print an error and request resumbit
                 pass
         else:
@@ -29,14 +32,16 @@ def index(request):
             # check whether it's valid:
             if form.is_valid():
                 # read the session from db
-                sessionId = 0
+                session = Session.objects.get(pk=form.cleaned_data['sessionCode'])
 
                 # create the new member of that session
+                member = models.Member.objects.create(name=form.cleaned_data['name'], session=session)
 
                 # return the rendered, new session page
                 return HttpResponseRedirect(reverse('session', args = [sessionId]))
 
             else:
+                print("FAIL2")
                 # TODO: print an error and request resumbit
                 pass
     else:
@@ -47,6 +52,6 @@ def index(request):
 
 def session(request, id):
     # get session from db
-    session = None
+    session = models.Session.objects.get(pk=id)
 
     return render(request, 'session.html', {'session': session})
